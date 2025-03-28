ce4cfb1920c1327d756f6d92f528d15b9a3de06a
Vector <harshitsharda0@gmail.com>
2025-03-28 14:19:15 +0530

    Public files not mounting fixed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 const entryRoutes = require('./routes/entryRoutes');

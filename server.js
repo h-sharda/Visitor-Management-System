fd8dcaa0b64179b9d96e98372c2feffa067a01fa
Vector <harshitsharda0@gmail.com>
2025-04-15 01:03:44 +0530

    Setting up environment for react

--- server.js
+++ server.js
@@ -23,23 +23,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 // Routes
 app.use("/user", userRoute);
 app.use("/", entryRoutes);
 
 // Serve the main page
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/signin", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "signin.html"));
-});
-
-app.get("/user-management", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "user-management.html"));
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
 
 // Start server
